0b780f9c18da309364ef5afc05fb04cdfca6826f
Ahmed Alsabag <ahmedalsabag@Ahmeds-MacBook-Pro.local>
2018-11-26 11:35:46 -0500

    More tests and changing chrome to defaut browser

---
+++ src/test/java/environment/EnvTest.java
@@ -0,0 +1,16 @@
+package environment;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+
+public class EnvTest {
+
+    @Test
+    public void CreateWebDriverTest(){
+        WebDriver test = Env.CreateWebDriver("chrome");
+        test.get("http://www.google.com/");
+        WebElement element = test.findElement(By.id("searchform"));
+        assertTrue(element.isDisplayed());
+
+    }
+}
