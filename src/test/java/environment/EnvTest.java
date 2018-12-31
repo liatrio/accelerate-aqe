de5026cb59c2979b94b8599d25e67678c7369610
Joe Knight <joe@devnill.com>
2018-12-31 15:37:08 -0600

    Close driver when test is finished

--- src/test/java/environment/EnvTest.java
+++ src/test/java/environment/EnvTest.java
@@ -3,14 +3,16 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.openqa.selenium.*;
 
-public class EnvTest {
+public class EnvTest{
 
     @Test
     public void CreateWebDriverTest(){
+        System.setProperty("Headless", "true");
         WebDriver test = Env.CreateWebDriver("chrome");
         test.get("http://www.google.com/");
         WebElement element = test.findElement(By.id("searchform"));
         assertTrue(element.isDisplayed());
 
+        test.close();
     }
 }
